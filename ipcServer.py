3e2ad202609057e20bf6bf7ee21914a3232223fc
SaranshSinha <31996102+SaranshSinha@users.noreply.github.com>
2019-11-25 01:45:23 +0530

    Update ipcServer.py

--- ipcServer.py
+++ ipcServer.py
@@ -1,16 +1,18 @@
-from multiprocessing.connection import Listener
-li=[]
-address = ('localhost', 6000)     # family is deduced to be 'AF_INET'
-listener = Listener(address, authkey='secret password')
-conn = listener.accept()
-print 'connection accepted from', listener.last_accepted
-while True:
-    msg = conn.recv()
-    li.append(msg)# do something with msg
-    if msg == 'close':
-			conn.send(li)
-			print("sending data in FIFO order...")
-        	#conn.close()
-			break
-print("data sent!")
-listener.close()
+import os
+import sys
+
+path = "/home/saransh/Desktop/port10.txt"
+path2 = "/home/saransh/Desktop/port11.txt"
+#li=[]
+os.mkfifo(path)
+fifo = open(path, "r")
+for line in fifo:
+    print "Received: " + line,
+    #li.append(line)
+
+fifo.close()
+print()
+print("sending data to client....")
+fifo2=open(path2,"w")
+fifo2.write("fifo based ipc!")
+fifo2.close()
